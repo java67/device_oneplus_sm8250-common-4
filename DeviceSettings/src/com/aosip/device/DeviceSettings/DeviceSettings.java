7ef6c2c6ddf273a512e19e0f7f44fe81e5b18487
osm1019 <osm1019@gmail.com>
2021-01-26 21:31:37 -0800

    Remove panel mode

--- DeviceSettings/src/com/aosip/device/DeviceSettings/DeviceSettings.java
+++ DeviceSettings/src/com/aosip/device/DeviceSettings/DeviceSettings.java
@@ -51,13 +51,8 @@ public class DeviceSettings extends PreferenceFragment
 
 
     private static final String KEY_CATEGORY_GRAPHICS = "graphics";
-    public static final String KEY_SRGB_SWITCH = "srgb";
     public static final String KEY_HBM_SWITCH = "hbm";
-    public static final String KEY_DC_SWITCH = "dc";
-    public static final String KEY_DCI_SWITCH = "dci";
-    public static final String KEY_NIGHT_SWITCH = "night";
-    public static final String KEY_WIDECOLOR_SWITCH = "widecolor";
-
+    public static final String KEY_DC_SWITCH = "dc";  
     private static final String KEY_CATEGORY_REFRESH = "refresh";
     public static final String KEY_REFRESH_RATE = "refresh_rate";
     public static final String KEY_AUTO_REFRESH_RATE = "auto_refresh_rate";
